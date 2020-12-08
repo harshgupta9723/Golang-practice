efff587cfb6795ce6ac83e3562eafc584090b8ff
Harsh Gupta <53069767+harshgupta9723@users.noreply.github.com>
2020-12-08 22:20:21 +0530

    Add files via upload

---
+++ practice.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main(){
+	
+	// declaring variables
+	x,y := 5,6
+	fmt.Println("x + y = ",x + y)
+	fmt.Println("hello world !")
+
+	// pointers
+	p := 10
+	fmt.Println(p) //variable 
+	fmt.Println(&p)//address
+
+}
